fac42cd5b63f2136a8048bc2e4f862b0f36a6506
newbiehre <jodichan.hk@gmail.com>
2023-06-16 23:13:23 +0800

    implement password encryption

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
 import { User } from '../user/user.entity';
 import { SigninDto, SignupDto } from '../user/user.request.dto';
+import { UserService } from '../user/user.service';
+
+const scrypt = promisify(_scrypt);
 
 export interface CurrentUserJwt {
   sub: string;
@@ -28,14 +32,34 @@ export class AuthService {
     };
   }
 
-  async signup(body: SignupDto) {
-    const newUser = await this.userService.create(body);
-    return this.generateJwtToken(newUser);
+  async signup({ email, firstName, lastName, password }: SignupDto) {
+    const existingUser = await this.userService.findByEmail(email);
+    if (existingUser)
+      throw new BadRequestException('User exists with email: ' + email);
+
+    // hash pw
+    const salt: string = randomBytes(8).toString('hex'); // randomBytes= raw binary (1 & 0)s; 8= 8 bytes of data which turns to 2 char when turned into hex; 16 char long string
+    const hash = (await scrypt(password, salt, 32)) as Buffer; // 32= gives 32 chars as output of scrypt
+    const hashedPassword = salt + '.' + hash.toString('hex');
+
+    // create new user & save it
+    const user = await this.userService.create({
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    });
+    return this.generateJwtToken(user);
   }
 
   async signin({ email, password }: SigninDto) {
     const existingUser = await this.userService.findByEmail(email);
-    if (existingUser?.password !== password) throw new UnauthorizedException();
+
+    const [salt, storedHash] = existingUser.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex'))
+      throw new BadRequestException('Bad password');
+
     return this.generateJwtToken(existingUser);
   }
 }
